d6ff265f2752fdaf08b24e08794e0dd9e07bbd68
Hackbright Student <info@hackbrightacademy.com>
2016-04-26 17:56:08 -0700

    Grade Listing for Student completed

--- hackbright-web.py
+++ hackbright-web.py
@@ -11,10 +11,13 @@ def get_student():
 
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
+    projects = hackbright.get_grades_by_github(github)
+
     html = render_template("student_info.html",
                            first=first,
                            last=last,
-                           github=github)
+                           github=github,
+                           projects=projects)
     return html
 
 
