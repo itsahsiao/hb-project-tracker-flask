84c9751434a521802592048c8acf8d3b130f3360
Hackbright Student <info@hackbrightacademy.com>
2016-04-26 17:24:38 -0700

    Route /student-add working properly with a link per instructions

--- hackbright-web.py
+++ hackbright-web.py
@@ -24,6 +24,7 @@ def get_student_form():
 
     return render_template("student_search.html")
 
+
 @app.route("/student-add", methods=['GET'])
 def student_add():
     """Add a student."""
@@ -31,19 +32,32 @@ def student_add():
     return render_template("student_add.html")
 
 
-@app.route("/student-add", methods=['POST'])
-def display_student_add():
-    """Display student info."""
+@app.route("/student-add-confirmation", methods=['POST'])
+def student_add_confirmation():
+    """Confirm student has been added."""
 
     first = request.form.get('firstname')
     last = request.form.get('lastname')
     github = request.form.get('github')
     hackbright.make_new_student(first, last, github)
-    
-    return render_template("student_info.html",
-                           first=first,
-                           last=last,
-                           github=github)
+
+    return render_template("student_add_confirmation.html",
+                            github=github)
+
+# # Keeps /student-add route, but two methods based on form submission
+# @app.route("/student-add", methods=['POST'])
+# def display_student_add():
+#     """Display student info."""
+
+#     first = request.form.get('firstname')
+#     last = request.form.get('lastname')
+#     github = request.form.get('github')
+#     hackbright.make_new_student(first, last, github)
+
+#     return render_template("student_info.html",
+#                            first=first,
+#                            last=last,
+#                            github=github)
 
 
 if __name__ == "__main__":
